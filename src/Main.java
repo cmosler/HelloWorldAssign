3b4c9dd4d9a51fad2b159e5595e846136e7fb437
Clinton Mosler <clintmos.16@gmail.com>
2023-09-16 16:40:55 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+    Clinton Molser
+    CSET 3600
+    Don Miller
+    Homework Assignment 1
+    09/16/23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
